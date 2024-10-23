5387797fc00c078d9f8e86c10668a92552e5625d
mfusik <mfusik@S201K13>
2024-10-23 07:46:38 +0200

    Dodaj pliki projektów.

---
+++ employeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadrowa
+{
+    internal class employeeModel
+    {
+        public Int32 employeeId { get; set; }
+        public string employeeName { get; set; }
+        public string employeeSurname { get; set; }
+        public float employeeSalary { get; set; }
+        public DateTime employeeDateOfEmployment { get; set; }
+        public DateTime? employeeDateOfTermination { get; set; }
+
+    }
+}
